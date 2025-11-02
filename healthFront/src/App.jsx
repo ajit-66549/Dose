e9d3afd148b9ac8a44102dfb910e8375a2e73b91
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-11-02 00:55:14 -0500

    Page routing for Dashboard and Set goal page

--- healthFront/src/App.jsx
+++ healthFront/src/App.jsx
@@ -1,10 +1,19 @@
-import Dashboard from './pages/Dashboard.jsx';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import SetGoals from "./pages/SetGoals.jsx";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <Router>
+      <nav>
+        <Link to="/">Dashboard</Link> | <Link to="/setGoals">Set Goals</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/setGoals" element={<SetGoals />} />
+      </Routes>
+    </Router>
   );
 }
 
